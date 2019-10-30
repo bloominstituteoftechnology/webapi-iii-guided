2b038636185e13071b1505fc1935b731d9fa112d
John Watt <j.watt10@gmail.com>
2019-10-30 10:21:33 -0700

    custom middleware

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+
+function logger (req,res,next) {
+  console.log(`The Logger: [${new.Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
+}
+
+server.use(helmet())
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
