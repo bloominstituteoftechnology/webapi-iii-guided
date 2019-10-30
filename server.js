7b060fc9d980d42fb02b6da23d1bcade84d2443c
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-30 11:05:53 -0600

    web-api-iii guided

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet')
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+// three amigos
+function dateLogger(req, res, next) {
+  console.log(new Date().toISOString())
+  next()
+}
+server.use('/', function (req, res, next) {
+  console.log(res, req.originalUrl)
+  next()
+})
 
+//global middleware
+server.use(helmet()); //third party
+server.use(express.json()); // built in
+server.use(dateLogger) // custom middleware
 server.use('/api/hubs', hubsRouter);
+
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
