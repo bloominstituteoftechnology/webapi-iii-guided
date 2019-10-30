b82a6bc98d7711194259691a99086fac90632194
andrew15722 <andrew15722@yahoo.com>
2019-10-30 10:03:51 -0700

    made the middleware to log the url and the http method

---
+++ notes.md
@@ -0,0 +1,25 @@
+# Middleware notes
+
+## Jargon
+
+Separation of Concerns
+
+We do not write code for the computer, code is a communication device,
+a way to reveal our intentions to the next developer.
+
+** everything is Middleware!! **
+
+- Almost everything
+
+## Types (based on how we got it or who built it)
+
+- built-in: included with express. ex: 'express.json()'
+- third party: must be installed from 'npm'
+- custom we code these.
+
+## Types (based on how its being used.)
+
+- global: runs on every request
+-
+
+- order matters, it goes top to bottom and left to right
--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+// The three amigos
+function dateLogger(req, res, next) {
+	console.log(new Date().toISOString());
+
+	next();
+}
+
+function logStuff(req, res, next) {
+	console.log(req.method, req.path);
+
+	next();
+}
+
+// global middleware
+server.use(helmet()); // third party middleware
+server.use(express.json()); // built-in middleware
+server.use(dateLogger);
+server.use(logStuff);
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+	const nameInsert = req.name ? ` ${req.name}` : '';
 
-  res.send(`
+	res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
