0c29f811d18f1e6fcd9ad13a60e7390bb4ee2f9a
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-10-30 13:10:01 -0400

    basic logger

--- server.js
+++ server.js
@@ -17,4 +17,12 @@ server.get('/', (req, res) => {
     `);
 });
 
+function logger(req, res, next) {
+    console.log(
+    `[${new Date().toISOString()}} ${req.method} to ${req.url} from ${req.get('Origin')}]`    
+    )
+    next()
+}
+
+
 module.exports = server;
